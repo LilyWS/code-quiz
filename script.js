51e865d4a127eca398e82cbac8c0804841d9aa80
LilyWS <stranger1212ss23@gmail.com>
2021-08-26 13:22:46 -0400

    Fininished highscore mechanic and webpage

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ var questionEl = document.querySelector(".question");
 var answersEl = document.querySelector(".answers");
 var feedbackEl = document.querySelector(".feedback");
 var timerEl = document.querySelector("#time");
+var highscoreDisplayEl = document.querySelector('.highscores')
 
 var questions = ["What language is making a framework for this website to work off of?", "What language is styling this website?", "What does DOM stand for?", "What framework allows for convinient DOM manipulation?", "The ____ tag tell search engines about your website"];
 
+
+var testRecieve = JSON.parse(localStorage.getItem("test"));
 var answers = [
     ["HTML", "CSS", "JQuery", "Python"],
     ["CSS", "Javascript", "Bootstrap", "Scratch"],
@@ -19,6 +22,49 @@ var question = -1;
 var qOrder = [0, 1, 2, 3, 4]; //question order
 qOrder = shuffleArray(qOrder);
 
+var highscores = JSON.parse(localStorage.getItem("highscores"));
+
+if(highscoreDisplayEl){
+    if(highscores!==null){
+        for(let i=0; i<highscores.length; i++){
+            let tempLi = document.createElement('li');
+            tempLi.textContent = highscores[i].initials + ": " + highscores[i].score;
+            highscoreDisplayEl.children[0].append(tempLi);
+
+        }
+    }
+}else{
+
+    //when a button is clicked in the answers section we either start the game or perform answer logic
+    document.querySelector('.answers').addEventListener('click', function(e) {
+        let clicked = e.target;
+        if(clicked.tagName == 'BUTTON'){
+            if(clicked.id == 'start'){
+                //start code here
+                question = -1;
+                qOrder = [0, 1, 2, 3, 4]; //question order
+                qOrder = shuffleArray(qOrder);
+                timeLeft=60;
+                timer();
+                timer = setInterval(timer, 1000);
+                renderQuestion();
+            }else if(clicked.id == 'submit'){
+                submitScore();
+            }else{ //it must be an answer
+
+                if(clicked.textContent == answers[selected][0]){
+                    feedbackEl.innerHTML = "<hr>Correct!"
+                    renderQuestion();
+                }else{
+                    feedbackEl.innerHTML = "<hr>Wrong!";
+                    timeLeft-=4;
+                    document.getElementById("time").children[0].textContent = timeLeft;
+                    renderQuestion();
+                }
+            }
+        }
+    });
+}
 function renderQuestion(){
     question += 1;
     if(question < questions.length){
@@ -51,7 +97,10 @@ function renderAnswers(selected){
 
 function renderScore() {
     questionEl.textContent="Good Job! Your score was " + timeLeft;
-    answersEl.innerHTML="<form><input defaultValue='Initials Here'></input><button>Submit Score</button></form>"
+    answersEl.innerHTML="<form><input placeholder='Initials Here'></input><button id='submit'>Submit Score</button></form>";
+    clearInterval(timer);
+    feedbackEl.textContent='';
+
 }
 
 function timer() {
@@ -63,35 +112,15 @@ function timer() {
     timeLeft -= 1;
 }
 
-
-//when a button is clicked in the answers section we either start the game or perform answer logic
-document.querySelector('.answers').addEventListener('click', function(e) {
-    let clicked = e.target;
-    if(clicked.tagName == 'BUTTON'){
-        if(clicked.id == 'start'){
-            //start code here
-            question = -1;
-            qOrder = [0, 1, 2, 3, 4]; //question order
-            qOrder = shuffleArray(qOrder);
-            timeLeft=60;
-            timer();
-            timer = setInterval(timer, 1000);
-            renderQuestion();
-        }else{ //it must be an answer
-
-            if(clicked.textContent == answers[selected][0]){
-                feedbackEl.innerHTML = "<hr>Correct!"
-                renderQuestion();
-            }else{
-                feedbackEl.innerHTML = "<hr>Wrong!";
-                timeLeft-=4;
-                document.getElementById("time").children[0].textContent = timeLeft;
-                renderQuestion();
-            }
-        }
+function submitScore(){
+    let submission = {
+        score: timeLeft,
+        initials: document.querySelector('input').value
     }
-});
-
+    highscores.push(submission);
+    console.log(highscores)
+    localStorage.setItem("highscores", JSON.stringify(highscores))
+}
 
 
 
